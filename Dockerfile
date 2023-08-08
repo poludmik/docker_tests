783ea26d17f93711c9f8602ab9158725a55664b6
Ivan Menshikov <menshiva@fit.cvut.cz>
2023-08-08 20:05:22 +0200

    Proper DB connection
    Use networks in docker-compose.yml
    Rewrite Dockerfile
    Refactor main.py: Proper database connection when running locally and when running in docker + add some SELECT examples

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,9 @@ FROM python:latest
 # RUN python3 -m venv $VIRTUAL_ENV
 # ENV PATH="$VIRTUAL_ENV/bin:$PATH"
 
-COPY requirements.txt .
-RUN pip3 install -r requirements.txt
-
 COPY . /folder
-
 WORKDIR /folder
 
-CMD [ "python3", "src/main.py"]
+RUN pip3 install -r requirements.txt
+
+ENTRYPOINT ["python3", "src/main.py", "-docker"]
--- src/main.py
+++ src/main.py
@@ -1,28 +1,53 @@
+import sys
 import numpy as np
 import psycopg2
 
-
 if __name__ == "__main__":
-    conn = psycopg2.connect(
-        host="localhost",
-        # url=,
-        # database="postgresql",
-        user="testuser",
-        password="pleaseletmein",
-        port=5432,
-        )
+    db_connection_dict = {
+        'dbname': 'postgres',
+        'user': 'testuser',
+        'password': 'pleaseletmein',
+    }
 
+    if len(sys.argv) == 2 and sys.argv[1] == '-docker':
+        # this script was run from docker
+        db_connection_dict['host'] = 'db'
+        pass
+    else:
+        # this script was run locally
+        db_connection_dict['host'] = 'localhost'
+        db_connection_dict['port'] = 5432
+        pass
+
+    conn = psycopg2.connect(**db_connection_dict)
     cur = conn.cursor()
-	# execute a statement
-    print('PostgreSQL database version:')
+
+    # db version
     cur.execute('SELECT version()')
     db_version = cur.fetchone()
-    print(db_version)
+    print(f'PostgreSQL database version: {db_version}')
+    print()
+
+    # retrieve all users ONE BY ONE
+    cur.execute('SELECT * FROM user_account')
+    print('All users:')
+    for record in cur:
+        print(record)
+    print()
+
+    # retrieve IDs and usernames of all users with username of length "username_len" AT ONCE
+    username_len = 4
+    cur.execute('SELECT acc.id, acc.username FROM user_account acc WHERE length(acc.first_name)=%s', (username_len,))
+    # equals to cur.execute('SELECT acc.id, acc.username FROM user_account acc WHERE length(acc.first_name)=4')
+    print(f'Users with usernames of length {username_len}:')
+    print(cur.fetchall())
+
     cur.close()
 
+    # don't forget to close the connection too!!!!!!
+    conn.close()
 
     # while(True):
     #     print("where number")
     #     n = int(input())
     #     print(np.random.rand(1, n))
-
